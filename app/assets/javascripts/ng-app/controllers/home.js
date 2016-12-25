dfc7935a52ded7febeee6700bf2d26172dc285e9
github username <github email address>
2016-12-25 13:31:43 +0200

    added styling and layout to home page

--- app/assets/javascripts/ng-app/controllers/home.js
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -3,8 +3,10 @@ function HomeController ($scope, $location) {
   $scope.goTo = function(place) {
     $location.path(`/${place}`);
   }
-
+  $scope.image = "https://s29.postimg.org/pkwylq9g7/blefs8_wzhm_breather.jpg";
+  $scope.bodyStyle = {background: "url(" + $scope.image + ") no-repeat center / cover"};
 }
+
 
 angular
   .module('app')
